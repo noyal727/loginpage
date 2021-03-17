c0b0cb662165e225ecc2e87ac108dfa99087904d
Jonnalagadda Noyal <jonnalagaddanoyal@Jonnalagaddas-MacBook-Pro.local>
2021-03-17 12:28:32 +0530

    loginPage

--- src/main/java/com/gateway/quinbook/client/ClientService.java
+++ src/main/java/com/gateway/quinbook/client/ClientService.java
@@ -3,7 +3,7 @@ import com.gateway.quinbook.dto.RegisterRequestDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(value = "register-details", url = "http://10.177.68.6:8081")
+@FeignClient(value = "register-details", url = "http://10.177.68.4:8081")
 public interface ClientService {
     @RequestMapping(method= RequestMethod.POST, path="/register")
     void registerUser(@RequestBody RegisterRequestDTO registerRequestDTO);
